453f218abb525f112fb3cc181b38b96ae9063730
carolinagomss <gomes_carolina.s@hotmail.com>
2021-03-28 12:54:09 -0300

    Criação de tela para resultado de conta telefonica; inclusao de chave/valor em lista de string; alteraçao de manifesto para navegacao de telas

--- checkpoint01/app/src/main/java/br/com/fiap/checkpoint01/ContaTelefonicaActivity.kt
+++ checkpoint01/app/src/main/java/br/com/fiap/checkpoint01/ContaTelefonicaActivity.kt
@@ -1,11 +1,50 @@
 package br.com.fiap.checkpoint01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_calculadora.*
+import kotlinx.android.synthetic.main.activity_conta_telefonica.*
 
 class ContaTelefonicaActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conta_telefonica)
+
+        btnContaCalcular.setOnClickListener {
+            val signatureValue = valorAssinatura.text.toString().trim().toFloat()
+            val localCall = chamadaLocal.text.toString().trim().toFloat() * 0.04
+            val cellCall = chamadaCelular.text.toString().trim().toFloat() * 0.20
+            val total = signatureValue + localCall + cellCall
+
+            val mIntent = Intent(this, ResultadoContaTelefoneActivity::class.java)
+            mIntent.putExtra("totalSignature", signatureValue)
+            mIntent.putExtra("totalLocal", localCall)
+            mIntent.putExtra("totalCell", cellCall)
+            mIntent.putExtra("valueTotal", total)
+            startActivity(mIntent)
+        }
+    }
+
+    private fun validateFields(): Boolean {
+        val signatureValue = valorAssinatura.text.toString().trim()
+        val localCall = chamadaLocal.text.toString().trim()
+        val cellCall = chamadaCelular.text.toString().trim()
+
+        if (signatureValue.isEmpty()) {
+            valorAssinatura.error = "Campo obrigatório"
+            valorAssinatura.requestFocus()
+            return false
+        } else if (localCall.isEmpty()) {
+            chamadaLocal.error = "Campo obrigatório"
+            chamadaLocal.requestFocus()
+            return false
+        } else if (cellCall.isEmpty()) {
+            chamadaCelular.error = "Campo obrigatório"
+            chamadaCelular.requestFocus()
+        }
+        return true
     }
 }
--- checkpoint01/app/src/main/java/br/com/fiap/checkpoint01/ResultadoContaTelefoneActivity.kt
+++ checkpoint01/app/src/main/java/br/com/fiap/checkpoint01/ResultadoContaTelefoneActivity.kt
@@ -1,4 +1,33 @@
 package br.com.fiap.checkpoint01
 
-class ResultadoContaTelefoneActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_resultado_conta_telefone.*
+
+class ResultadoContaTelefoneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_conta_telefone)
+
+        val resultSignature = intent.getFloatExtra("totalSignature", 0f)
+        val resultTotalLocal = intent.getDoubleExtra("totalLocal", 0.0)
+        val resultTotalCell = intent.getDoubleExtra("totalCell", 0.0)
+        val resultValueTotal = intent.getDoubleExtra("valueTotal", 0.0)
+
+        val txvResultSignature = String.format("%.2f", resultSignature)
+        val txvResultTotalLocal = String.format("%.2f", resultTotalLocal)
+        val txvResultTotalCell = String.format("%.2f", resultTotalCell)
+        val txvResultValueTotal = String.format("%.2f", resultValueTotal)
+
+        txvValorAssinatura.setText(txvResultSignature)
+        txvValorChamadaLocal.setText(txvResultTotalLocal)
+        txvValorChamadaCelular.setText(txvResultTotalCell)
+        txvValorTotal.setText(txvResultValueTotal)
+    }
+
+    override fun finish() {
+        super.finish()
+    }
 }
